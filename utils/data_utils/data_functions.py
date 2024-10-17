7eac9dfd04b0c3f7eadcc7c1a410a72272857ac4
Enot3811 <enot3811@gmail.com>
2024-10-17 20:17:42 +0300

    Move noise function to data utils module

--- utils/data_utils/data_functions.py
+++ utils/data_utils/data_functions.py
@@ -1,13 +1,15 @@
 """Module that contain tool functions for working with images."""
 
 
-from typing import List, Union, Optional, Tuple
+from typing import List, Union, Optional, Tuple, Callable
+
 import matplotlib.axes
 import numpy as np
 from numpy.typing import NDArray
 import cv2
 import matplotlib
 import matplotlib.pyplot as plt
+import torch
 
 
 def show_images_cv2(
@@ -108,3 +110,34 @@ def show_image_plt(
     if plt_show:
         plt.show()
     return ax
+
+# TODO docstring
+def gather(consts: torch.Tensor, t: torch.Tensor):
+    """Gather consts for $t$ and reshape to feature map shape"""
+    c = consts.gather(-1, t)
+    return c.reshape(-1, 1, 1, 1)
+
+
+def make_noise_generator(beta) -> Callable:
+    alpha = 1. - beta
+    alpha_bar = torch.cumprod(alpha, dim=0)
+    def generate_noise(x0, t):
+        mean = gather(alpha_bar, t) ** 0.5 * x0
+        var = 1 - gather(alpha_bar, t)
+        eps = torch.randn_like(x0, device=x0.device)
+        return mean + (var ** 0.5) * eps, eps # also returns noise
+    return generate_noise
+
+
+def make_denoiser(beta) -> Callable:
+    alpha = 1. - beta
+    alpha_bar = torch.cumprod(alpha, dim=0)
+    def denoise_sample(xt, noise, t):
+        alpha_t = gather(alpha, t)
+        alpha_bar_t = gather(alpha_bar, t)
+        eps_coef = (1 - alpha_t) / (1 - alpha_bar_t) ** .5
+        mean = 1 / (alpha_t ** 0.5) * (xt - eps_coef * noise)
+        var = gather(beta, t)
+        eps = torch.randn(xt.shape, device=xt.device)
+        return mean + (var ** 0.5) * eps
+    return denoise_sample
