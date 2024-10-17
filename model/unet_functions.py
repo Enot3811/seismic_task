43ba48913cfb861afc241649c61a75610a2fc37b
Enot3811 <enot3811@gmail.com>
2024-10-17 21:00:52 +0300

    Update train

--- model/unet_functions.py
+++ model/unet_functions.py
@@ -10,80 +10,90 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 import albumentations as A
+from loguru import logger
 
 from model.unet import UNet
-from utils.data_utils.data_functions import make_noise_generator, 
+from utils.data_utils.data_functions import make_noise_generator
 from utils.segy_utils.segy_slice_dataset import SegySliceDataset
 
 
-# TODO всё переписать
-def train_unet(config: Dict[str, Any]):
+# TODO добавить логгер
+def train_unet(config: Dict[str, Any], model: torch.nn.Module):
 
-    # Training params
-    batch_size = 16
-    lr = 2e-4
-    end_lr = 2e-6
-    epochs = 5
-    device = torch.device('cuda')
-    save_loss_step = 25
-    save_path = Path('trains/train6')
+    # Device
+    if config['device'] == 'cuda':
+        if torch.cuda.is_available():
+            device = torch.device('cuda')
+        else:
+            logger.warning('Cuda is not available. Switching device to "CPU".')
+            device = torch.device('cpu')
+    elif config['device'] == 'cpu':
+        device = torch.device('cpu')
+    else:
+        raise ValueError('Wrong device')
+    
+    # Random
+    torch.random.manual_seed(config['random_seed'])
+    
+    # Directories
+    if config['save_path'].exists():
+        input(f'{str(config['save_path'])} already exists. Continue to rewrite it.')
+        shutil.rmtree(config['save_path'])
+    tensorboard_dir = config['save_path'] / 'tensorboard'
+    ckpt_dir = config['save_path'] / 'ckpts'
+    tensorboard_dir.mkdir(parents=True)
+    ckpt_dir.mkdir(parents=True)
+
+    # Tensorboard
+    log_writer = SummaryWriter(str(tensorboard_dir))
 
     # Noise generator
-    n_steps = 100
-    beta = torch.linspace(0.0001, 0.04, n_steps, device=device)
+    beta = torch.linspace(0.0001, 0.04, config['noise_steps'], device=device)
     make_noise = make_noise_generator(beta)
 
+    # Augmentations
+    if config['crop_size'] or config['mean_std']:
+        transforms = []
+        if config['crop_size']:
+            transforms.append(A.RandomCrop(*config['crop_size']))
+        if config['mean_std']:
+            transforms.append(A.Normalize(*config['mean_std'],
+                                          max_pixel_value=1))
+        transforms = A.Compose(transforms=transforms)
+    else:
+        transforms=None
+
     # Datasets and dataloaders
-    crop_size = (224, 224)
-    transforms = A.Compose(transforms=[
-        A.RandomCrop(*crop_size),
-        A.Normalize(mean=(0.5,), std=(0.19,), max_pixel_value=1)
-    ])
-    sgy_path = '../data/seismic/seismic.sgy'
-    train_axes = (1,)
-    train_dset = SegySliceDataset(sgy_path, train_axes, transforms=transforms)
-    train_loader = DataLoader(
-        train_dset, batch_size, shuffle=True, drop_last=True)
-    val_axes = (0,)
-    val_dset = SegySliceDataset(sgy_path, val_axes, transforms=transforms)
-    val_loader = DataLoader(
-        val_dset, batch_size, shuffle=True, drop_last=True)
-    val_save_loss_step = (math.ceil(len(val_loader) /
-                          math.ceil(len(train_loader) / save_loss_step)))
+    train_dset = SegySliceDataset(
+        config['sgy_path'], config['train_axes'], transforms=transforms)
+    val_dset = SegySliceDataset(
+        config['sgy_path'], config['val_axes'], transforms=transforms)
+
+    train_loader = DataLoader(train_dset, config['batch_size'], shuffle=True)
+    val_loader = DataLoader(val_dset, config['batch_size'], shuffle=True)
 
     # Model
     unet = UNet(image_channels=1, n_channels=32).to(device=device)
 
     # Optimizer
-    optimizer = torch.optim.AdamW(unet.parameters(), lr=lr)
+    optimizer = torch.optim.AdamW(unet.parameters(), lr=config['start_lr'])
     lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(
-        optimizer, T_max=epochs, eta_min=end_lr)
+        optimizer, T_max=config['epochs'], eta_min=config['end_lr'])
 
-    # Prepare for training
-    if save_path.exists():
-        input(f'{str(save_path)} already exists. Continue to rewrite it.')
-        shutil.rmtree(save_path)
-    tensorboard_dir = save_path / 'tensorboard'
-    ckpt_dir = save_path / 'ckpts'
-    tensorboard_dir.mkdir(parents=True)
-    ckpt_dir.mkdir(parents=True)
-
-    log_writer = SummaryWriter(str(tensorboard_dir))
-
-    # Train
+    # Do train
     best_metric = None
-    for e in range(epochs):
+    for e in range(config['epochs']):
 
         # Train step
-        losses = []
-        t_train_losses = []
+        train_losses = []
         pbar = tqdm(train_loader, desc=f'Epoch {e} train')
         for i, batch in enumerate(pbar):
             batch = batch.to(device=device)
 
             # Noise samples
-            t = (torch.randint(0, n_steps, (batch_size,), dtype=torch.long)
-                 .to(device=device))  # Random 't's
+            t = (torch.randint(
+                0, config['noise_steps'], (config['batch_size'],),
+                dtype=torch.long).to(device=device))  # Random 't's
             batch_noised, noise = make_noise(batch, t)
 
             pred_noise = unet(batch_noised, t)
@@ -92,49 +102,38 @@ def train_unet(config: Dict[str, Any]):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            losses.append(loss.item())
-
-            if i % save_loss_step == 0 or i == len(train_loader):
-                train_loss = torch.mean(torch.tensor(losses))
-                pbar.set_postfix({'loss': f'{train_loss.item():.4f}'})
-                t_train_losses.append(train_loss)
-                losses.clear()
+            train_losses.append(loss.item())
 
         # Val step
         with torch.no_grad():
             unet.eval()
-            losses.clear()
-            t_val_losses = []
+            val_losses = []
             pbar = tqdm(val_loader, desc=f'Epoch {e} val')
             for i, batch in enumerate(pbar):
                 batch = batch.to(device=device)
 
                 # Noise samples
-                t = (torch.randint(0, n_steps, (batch_size,), dtype=torch.long)
-                        .to(device=device))  # Random 't's
+                t = (torch.randint(
+                    0, config['noise_steps'], (config['batch_size'],),
+                    dtype=torch.long).to(device=device))  # Random 't's
                 batch_noised, noise = make_noise(batch, t)
 
                 pred_noise = unet(batch_noised, t)
 
                 loss = F.mse_loss(noise, pred_noise)
-                losses.append(loss)
-
-                if i % val_save_loss_step == 0 or i == len(val_loader):
-                    val_loss = torch.mean(torch.tensor(losses))
-                    pbar.set_postfix({'loss': f'{val_loss.item():.4f}'})
-                    t_val_losses.append(val_loss)
-                    losses.clear()
+                val_losses.append(loss)
 
         lr_scheduler.step()
         lr = lr_scheduler.get_last_lr()[0]
 
         # Log
-        for step, (train_loss, val_loss) in enumerate(zip(t_train_losses,
-                                                          t_val_losses)):
-            log_writer.add_scalars('loss', {
-                'train': train_loss,
-                'val': val_loss
-            }, global_step=e * len(train_loader) + step)
+        train_loss = torch.mean(torch.tensor(train_losses))
+        val_loss = torch.mean(torch.tensor(val_losses))
+
+        log_writer.add_scalars('loss', {
+            'train': train_loss,
+            'val': val_loss
+        }, global_step=e)
         log_writer.add_scalar('lr', lr, e)
         
         # Save model
@@ -152,6 +151,7 @@ def train_unet(config: Dict[str, Any]):
     log_writer.close()
 
 
+# TODO всё переписать
 def infer_on_noise(config: Dict[str, Any]):
     """Infer trained unet on pure noise."""
     ckpt_pth = 'trains/train5/ckpts/best_checkpoint.pth'
